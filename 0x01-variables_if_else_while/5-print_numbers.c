5484ed1d0670003da6a49e10d0a1f126c5dda26b
cynthiarichard007 <cynthiarichard007@gmail.com>
2022-09-12 08:12:20 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphabets
+ *
+ * Return: Always(Sucessful)
+ */
+int main(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+        putchar(c);
+}
+putchar('\n');
+return(0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+My first readme file for 0x01-variables if else while
