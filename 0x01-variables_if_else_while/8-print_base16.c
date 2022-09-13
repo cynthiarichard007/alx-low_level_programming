19c951cce19a7ac6edd5f392c2f4639a8357e347
cynthiarichard007 <cynthiarichard007@gmail.com>
2022-09-12 21:20:49 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Prints a hexadecimal string
- *
- * Return: Always(Sucessful)
+ * main - Entry point
+ * Description: Print base 16 numbers
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-{
-        putchar(c);
-}
-for (c = 'a'; c <= 'f'; c++)
-{
-	putchar(c);
-}
-putchar('\n');
-return(0)
+	char c;
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
