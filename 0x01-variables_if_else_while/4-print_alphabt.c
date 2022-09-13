105e1ea2f549dc9953a968f6c118897229ee814b
cynthiarichard007 <cynthiarichard007@gmail.com>
2022-09-12 21:01:04 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Prints lowercase alphabets
- *
- * Return: Always(Sucessful)
+ * main - Entry point
+ * Description: prints lowercase alphabets
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-char c;
-for (c = 'a' ; c <= 'z' ; c++)
-{
-if (c != 'e' && c !<= 'q')
-{
-        putchar(c);
-}
-}
-putchar('\n');
-return(0);
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+	return (0);
 }
