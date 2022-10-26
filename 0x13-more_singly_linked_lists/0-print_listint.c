83fda767688f85f2e06b450c0e5514d1b0868c9c
cynthiarichard007 <cynthiarichard007@gmail.com>
2022-10-25 20:17:50 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,10 +11,11 @@ size_t print_listint(const listint_t *h)
 	unsigned int i = 0;
 
 		while (h != NULL)
-				{
-							printf("%d\n", h->n);
-									h = h->next;
-											i++;
-												}
-			return (i);
+
+		{		
+			printf("%d\n", h->n);
+			h = h->next;
+			i++;
+		}		
+		return (i);
 }
